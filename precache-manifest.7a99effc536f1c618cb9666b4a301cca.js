5e92eea30c222b827890a8df006be9df80dee110
CHENG HAN <y3226999@gmail.com>
2020-09-18 00:59:47 +0800

    Updates

---
+++ precache-manifest.7a99effc536f1c618cb9666b4a301cca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661d1a3ac08f6c7fe46c8290482ecf0b",
+    "url": "/repo-list/index.html"
+  },
+  {
+    "revision": "b85299a2b03c6fa10e53",
+    "url": "/repo-list/static/js/2.117cd1cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repo-list/static/js/2.117cd1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26412b24a1fd8a8d7cfa",
+    "url": "/repo-list/static/js/main.716c99ed.chunk.js"
+  },
+  {
+    "revision": "b62981562b289ecf9840",
+    "url": "/repo-list/static/js/runtime-main.a07b86fc.js"
+  }
+]);
